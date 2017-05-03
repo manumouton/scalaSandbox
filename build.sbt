b62f6c34ec096853491515dc0c961f386721561c
Emmanuel Mouton <mou@foyer.lu>
2017-05-03 16:56:49 +0200

    Adding sbt info. Playing with Akka Actors

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "scalaSanbox"
+
+version := "1.0"
+
+scalaVersion := "2.11.11"
+
+libraryDependencies +=
+  "com.typesafe.akka" %% "akka-actor" % "2.5.1"
---
+++ src/main/scala/programmingScala/shapes/ShapesDrawingActor.scala
@@ -0,0 +1,26 @@
+package programmingScala.shapes
+
+object Messages {
+  object Exit
+  object Finished
+  case class Response(message: String)
+}
+
+import akka.actor.Actor
+
+class ShapesDrawingActor extends Actor {
+  import Messages._
+
+  def receive = {
+    case s: Shape =>
+      s.draw(str => println(s"ShapesDrawingActor: $str"))
+      sender ! Response(s"ShapesDrawingActor: $s drawn")
+      //equivalent to sender.!( Response( s" ShapesDrawingActor: $ s drawn"))
+    case Exit =>
+      println(s"ShapesDrawingActor: exiting...")
+    case unexpected =>
+      val response = Response(s"Error: Unknown message: £unexpected")
+      println(s"ShapesDrawingActor: $response")
+      sender ! response
+  }
+}
