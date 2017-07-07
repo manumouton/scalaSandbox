c83b71cb8eb989e8ff4e4b6ff6fed16b09036d88
Emmanuel Mouton <mou@foyer.lu>
2017-07-07 10:58:27 +0200

    Playing with number format / currency format

--- src/main/scala/programmingScala/currency/CurrencyFormatter.scala
+++ src/main/scala/programmingScala/currency/CurrencyFormatter.scala
@@ -1,18 +1,34 @@
 package programmingScala.currency
 
+import java.text.NumberFormat
+import java.util.Locale
+
 /**
   * Created by mou on 07/07/2017.
   */
 object CurrencyFormatter {
 
+  def getNumberFomatter(language:String, currency: String): NumberFormat = {
+    val numberFormat = NumberFormat.getCurrencyInstance(new Locale(language))
+    numberFormat.setCurrency(java.util.Currency.getInstance(currency))
+    numberFormat
+  }
+
   def main(args: Array[String]): Unit = {
     val currency = "EUR"
     val amount = 12569.555526399
 
-    val numberFormat = java.text.NumberFormat.getCurrencyInstance()
-    numberFormat.setCurrency(java.util.Currency.getInstance(currency))
+    val fr = "FR"
+    val us = "USA"
+    val china = "CHN"
 
-    println("Formatted amount = " + numberFormat.format(amount))
+    val frFormattedAmount = getNumberFomatter(fr, currency).format(amount)
+    val usFormattedAmount = getNumberFomatter(us, currency).format(amount)
+    val chineseFormattedAmount = getNumberFomatter(china, currency).format(amount)
+
+    println("FR Formatted amount = " + frFormattedAmount)
+    println("USA Formatted amount = " + usFormattedAmount)
+    println("Chinese Formatted amount = " + chineseFormattedAmount)
 
     val roundedAmount = BigDecimal(amount).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
 
