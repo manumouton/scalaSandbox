eda7da6e891f521a8753797d2943585c0f79ec03
manumouton <manumouton@gmail.com>
2017-08-20 15:28:29 +0200

    Making List covariant

--- src/main/scala/programmingScala/datastructures/List.scala
+++ src/main/scala/programmingScala/datastructures/List.scala
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException
 /**
   * Created by manu on 8/07/17.
   */
-trait List[T] {
+trait List[+T] {
 
   def isEmpty: Boolean
 
@@ -19,11 +19,11 @@ class Cons[T](val head:T, val tail:List[T]) extends List[T]{
 
   override def isEmpty: Boolean = false
 
-  def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
+  def singleton[T](elem: T) = new Cons[T](elem, Nil)
 
 }
 
-class Nil[T] extends List[T]{
+object Nil extends List[Nothing]{
 
   override def isEmpty: Boolean = true
 
@@ -32,10 +32,14 @@ class Nil[T] extends List[T]{
   override def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
+object test {
+  val x: List[String] = Nil
+}
+
 object List {
   //List(1,2) = List.apply(1,2)
-  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
-  def apply[T](): List[T] = new Nil[T]
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, Nil))
+  def apply[T](): List[T] = Nil
 
 }
 
@@ -46,7 +50,7 @@ object nth{
     else if (i == 0) xs.head
     else nth(i-1, xs.tail)
 
-  val list = new Cons(1, new Cons(2, new Cons(3, new Nil)))
+  val list = new Cons(1, new Cons(2, new Cons(3, Nil)))
 
   def main(args: Array[String]): Unit = {
     nth(2, list)
