599ae14ea3535d32bc60a80d88ef23dc5445ca8e
manumouton <manumouton@gmail.com>
2017-08-20 14:50:39 +0200

    Function everywhere

---
+++ src/main/scala/programmingScala/datastructures/List.scala
@@ -0,0 +1,56 @@
+package programmingScala.datastructures
+
+import java.util.NoSuchElementException
+
+/**
+  * Created by manu on 8/07/17.
+  */
+trait List[T] {
+
+  def isEmpty: Boolean
+
+  def head: T
+
+  def tail: List[T]
+
+}
+
+class Cons[T](val head:T, val tail:List[T]) extends List[T]{
+
+  override def isEmpty: Boolean = false
+
+  def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
+
+}
+
+class Nil[T] extends List[T]{
+
+  override def isEmpty: Boolean = true
+
+  override def head: Nothing = throw new NoSuchElementException("Nil.head")
+
+  override def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+object List {
+  //List(1,2) = List.apply(1,2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T](): List[T] = new Nil[T]
+
+}
+
+object nth{
+
+  def nth[T](i: Int, xs: List[T]): T =
+    if(xs.isEmpty) throw new IndexOutOfBoundsException
+    else if (i == 0) xs.head
+    else nth(i-1, xs.tail)
+
+  val list = new Cons(1, new Cons(2, new Cons(3, new Nil)))
+
+  def main(args: Array[String]): Unit = {
+    nth(2, list)
+    nth(4, list)
+    nth(-1, list)
+  }
+}
